64f6a7c758d725001ef879e73aa3f5b04e5b7741
karan <kg9811422163@gmail.com>
2021-07-20 01:53:25 +0530

    Updates

---
+++ precache-manifest.a19f1f36848564e6a26e659af93779ae.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ac15a85776f06ae6b63c262c9f832e9",
+    "url": "/karanghai9/index.html"
+  },
+  {
+    "revision": "d5d682f6f385eee18ad4",
+    "url": "/karanghai9/static/css/2.91044456.chunk.css"
+  },
+  {
+    "revision": "94458b79348915e78e01",
+    "url": "/karanghai9/static/css/3.7457a885.chunk.css"
+  },
+  {
+    "revision": "8f3c61dbe27a07ce31de",
+    "url": "/karanghai9/static/css/4.44ea4bca.chunk.css"
+  },
+  {
+    "revision": "539383447aaf6877308d",
+    "url": "/karanghai9/static/css/5.89245b60.chunk.css"
+  },
+  {
+    "revision": "279d90d1e963adb8694f",
+    "url": "/karanghai9/static/css/7.020bd4ad.chunk.css"
+  },
+  {
+    "revision": "a8003566a1b57d579fb8",
+    "url": "/karanghai9/static/css/main.ec2ab062.chunk.css"
+  },
+  {
+    "revision": "d5d682f6f385eee18ad4",
+    "url": "/karanghai9/static/js/2.5ac37102.chunk.js"
+  },
+  {
+    "revision": "774152ec3dfb5dadc6880f2e7f79e400",
+    "url": "/karanghai9/static/js/2.5ac37102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94458b79348915e78e01",
+    "url": "/karanghai9/static/js/3.febda60b.chunk.js"
+  },
+  {
+    "revision": "8f3c61dbe27a07ce31de",
+    "url": "/karanghai9/static/js/4.0eb87741.chunk.js"
+  },
+  {
+    "revision": "539383447aaf6877308d",
+    "url": "/karanghai9/static/js/5.bf5cd6ec.chunk.js"
+  },
+  {
+    "revision": "e1b166044ece9554d164",
+    "url": "/karanghai9/static/js/6.6656fe38.chunk.js"
+  },
+  {
+    "revision": "279d90d1e963adb8694f",
+    "url": "/karanghai9/static/js/7.69cf0ab4.chunk.js"
+  },
+  {
+    "revision": "a8003566a1b57d579fb8",
+    "url": "/karanghai9/static/js/main.bc42412e.chunk.js"
+  },
+  {
+    "revision": "0c2020a0cbd4f86ce2de",
+    "url": "/karanghai9/static/js/runtime-main.8a09d956.js"
+  },
+  {
+    "revision": "48778075ddc4c0b7d08ea32d8354ef3d",
+    "url": "/karanghai9/static/media/MyResume.48778075.pdf"
+  },
+  {
+    "revision": "4092268f27cac37fbe4665086595807c",
+    "url": "/karanghai9/static/media/mee.4092268f.png"
+  },
+  {
+    "revision": "8d8236d9e42b9a139a3e0a09449990b3",
+    "url": "/karanghai9/static/media/python.8d8236d9.jpg"
+  },
+  {
+    "revision": "7078d282800d085afb453a1df59085b3",
+    "url": "/karanghai9/static/media/reactJS.7078d282.jpg"
+  }
+]);
